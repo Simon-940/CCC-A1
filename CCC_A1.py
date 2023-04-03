c981c77d9876265364b9d3dbdea344b7cff6a4c7
Simon Gao <a605813435@gmail.com>
2023-04-03 23:33:27 +1000

    MPI implementation for task1

--- CCC_A1.py
+++ CCC_A1.py
@@ -1,3 +1,7 @@
+import collections
+import functools
+import operator
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -32,7 +36,7 @@ class Tweet():
 
     def get_author(self):
         return self.author
-def extract_data(tweets ,locations):
+def extract_data(tweets ,locations,size,start_point):
     capital_cities = {
         '1gsyd': 0,
         '2gmel': 0,
@@ -48,11 +52,12 @@ def extract_data(tweets ,locations):
 
     tweeters = {}
 
-    for tweet in tweets:
+    for tweet in tweets[start_point:start_point+size]:
         t = Tweet(tweet)
         city = t.get_place()
         id = t.get_author()
-        # check if the place from tweet matches anything in the locations data. If not from irrelevant area and match, increment count
+        # check if the place from tweet matches anything in the locations data. If not from irrelevant area and
+        # match, increment count
         if city in locations and locations[city]['gcc'] not in irrelevant_areas:
             gcc_code = locations[city]['gcc']
             capital_cities[gcc_code] += 1
@@ -67,28 +72,37 @@ def extract_data(tweets ,locations):
                 else:
                     tweeters[id][1][gcc_code] = 1
 
+    return capital_cities, tweeters
 
-    # t1
-    top_cities = sorted(capital_cities.items(), key=lambda item: item[1], reverse=True)
-    # print(top_cities)
-
-    # t2
-    t2top_tweeters = sorted(tweeters.items(), key=lambda item: item[1][0], reverse=True)[:10]
-    # print(t2top_tweeters)
-
-    # t3
-    t3top_tweeters = sorted(tweeters.items(), key=lambda item: len(item[1][1]), reverse=True)[:10]
-    # print(t3top_tweeters)
-
-    return [top_cities ,t2top_tweeters ,t3top_tweeters]
-
-
+# set MPI variable
 comm = MPI.COMM_WORLD
 rank = comm.rank
 size = comm.size
+# calculate chuck size using node size
+chuck_size = len(tweets)//size
+start_points = list(range(0, len(tweets), len(tweets)//size))[:size]
+sendbuf = start_points
+# scatter starting point
+start_point = comm.scatter(start_points, 0)
+res = extract_data(tweets, locations, chuck_size, start_point)
+recv = comm.gather(res, 0)
 
-res = extract_data(tweets ,locations)
-
-recv = comm.gather(res ,0)
 if rank == 0:
-    print(recv)
+    # dict list for tasks
+    cap = [item[0] for item in recv]
+    twt = [item[1] for item in recv]
+    # sum values with same key
+    res_city = dict(functools.reduce(operator.add, map(collections.Counter, cap)))
+    # TODO: task 2 and 3
+
+    # t1
+    top_cities = sorted(res_city.items(), key=lambda item: item[1], reverse=True)
+    print(top_cities, "\n")
+
+    # # t2
+    # t2top_tweeters = sorted(res_twt.items(), key=lambda item: item[1][0], reverse=True)[:10]
+    # print(t2top_tweeters, "\n")
+    #
+    # # t3
+    # t3top_tweeters = sorted(res_twt.items(), key=lambda item: len(item[1][1]), reverse=True)[:10]
+    # print(t3top_tweeters, "\n")
